a6add5dda85f5d3e6f10f52c94967420883b27fb
Naigege <Naichenzhao@gmail.com>
2014-03-13 19:11:41 -0400

    took out funky phasing to better match .xlsx template

--- R/inception/RolloutExecutable.R
+++ R/inception/RolloutExecutable.R
@@ -6,7 +6,7 @@
 source('~/code/networkplanner.R/R/inception/CommonRolloutFunctions.R') 
 
 #Set working directory to scenario of interest here
-setwd("~/Dropbox/Myanmar_GIS/Modeling/Tests/644-Chin-HHDem240-13c:KWh-10kWDslMin-$1.15Dsl-SN10/")
+setwd("~/Dropbox/Myanmar_GIS/Modeling/Tests/644-Chin-| HHDem240| LV15MV22|13 c:kWh|10kWDslMin | $1.15 Dsl|92G|385MG|512OG | SN10/")
 
 
 ##1.0 - Import DATA
@@ -42,53 +42,14 @@ nearsighted_grid_cumulatives <- downstream.sum.calculator(nearsighted_grid)
 farsighted_grid <- far_sighted_rollout(nearsighted_grid_cumulatives)
 #*****
 
-
-#5. Assign Phase bins by distance threshold to each settlement
-#But first, determine cummulative values
-
-#How many total new connections?? 
-new_grid_connections <- sum(farsighted_grid$Demand..household....Target.household.count)
-
-
-#Develop cummulative sum of network length metric
-farsighted_grid <- mutate(farsighted_grid, 
-                          CumulativeNetworkExtent.m = cumsum(dist),
-                          CumulativeHousesConnected.qty = cumsum(Demand..household....Target.household.count),
-                          MVLinePerConnection = dist/Demand..household....Target.household.count,
-                          TransformerCostPerConnection = System..grid....Transformer.cost/Demand..household....Target.household.count,
-                          PercentOfNewGridConnections = CumulativeHousesConnected.qty/new_grid_connections)
-
-#That lets us develop Phase bins
-farsighted_grid$PhaseByMVQuintile <- NA
-total_phases <- 5
-total_MV_dist <- sum(farsighted_grid$dist)
-
-for (j in 1:total_phases){
-  
-  lower_cutoff <- (j-1)/total_phases*total_MV_dist
-  upper_cutoff <- j/total_phases*total_MV_dist
-  
-  farsighted_grid$PhaseByMVQuintile[which((farsighted_grid$CumulativeNetworkExtent.m >= lower_cutoff) &
-                                (farsighted_grid$CumulativeNetworkExtent.m <= upper_cutoff))] <- j
-  
-}
-
 #output csv for analysess 
 write.csv(farsighted_grid, "metrics-local-grid-only-rollout_sequence.csv", row.names=F)
 
 #PAss through ALL metric local values
 shared_column_names <- colnames(local)[which(colnames(local) %in% colnames(farsighted_grid))]
 farsighted_grid_full <- merge(local, farsighted_grid, by = shared_column_names, all.x=T, all.y=T)
-
-#Order output by Phase
-farsighted_grid_full <- arrange(farsighted_grid_full, PhaseByMVQuintile)
-#Correct for NA Phases in Mini and Off grid systems
-farsighted_grid_full$PhaseByMVQuintile[which(farsighted_grid_full$Metric...System == 'mini-grid')] <- 'MiniGrid Systems'
-farsighted_grid_full$PhaseByMVQuintile[which(farsighted_grid_full$Metric...System == 'off-grid')] <- 'OffGrid Systems'
-farsighted_grid_full$PhaseByMVQuintile[which(farsighted_grid_full$Metric...System == 'unelectrified')] <- 'Pre-Electrified'
-
 #Output it all with useful phases
-write.csv(farsighted_grid_full, "metrics-local-rollout_sequence-FULL.csv", row.names=F)
+write.csv(farsighted_grid_full, "metrics-local-all-nodes-rollout_sequence.csv", row.names=F)
 
 # #Output a new shapefile with all the attirbute data of interest
 #remove multiple nodes on line segments
