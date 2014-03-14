f5a12b9bb357e6b149acc8ebc1cdf52aab88e11c
Naigege <Naichenzhao@gmail.com>
2014-03-14 12:02:19 -0400

    adapting work with Carbz to exactly mimic template inputs needed

--- R/inception/RolloutExecutable.R
+++ R/inception/RolloutExecutable.R
@@ -8,9 +8,7 @@ source('~/code/networkplanner.R/R/inception/CommonRolloutFunctions.R')
 #Set working directory to scenario of interest here
 setwd("~/Dropbox/Myanmar_GIS/Modeling/Tests/644-Chin-| HHDem240| LV15MV22|13 c:kWh|10kWDslMin | $1.15 Dsl|92G|385MG|512OG | SN10/")
 
-
-##1.0 - Import DATA
-#Import metrics.local for only grid-proposed nodes -> local.grid
+##1.0 - Import metrics.local for only grid-proposed nodes -> local.grid
 #load metrics.local to associated settlement points with proposed grid data
 local <- read.csv("metrics-local.csv", skip=1) #RUNTIME ~ 00:28 mins
 local$Settlement.id <- rownames(local) #use generic row names for unique ID of each unique settlement point
@@ -20,11 +18,11 @@ proj4 <- read.csv("metrics-local.csv", nrows=1, header = FALSE)
 proposed <- readShapeLines("networks-proposed.shp") #RUNTIME ~ 00:08 mins
 
 
-#2. Use output of priortized.grid function as input to far-sighted optimized rollout algorithim 
+#Use output of priortized.grid function as input to far-sighted optimized rollout algorithim 
 #takes a shapefile (network) and csv (nodal descriptions and weights) 
 #and suggests a sequential, phased roll-out of the system based on a greedy, one step ahead view
 #***RUNTIME ~08:00***********
-nearsighted_grid <- prioritized.grid.nearsighted(local, proposed, proj4)
+nearsighted_grid <- prioritized.grid.nearsighted(local,proposed, proj4)
 ##***************************
 
 #Explicitly define greedy grid output as a dataframe
@@ -34,25 +32,24 @@ if (length(nearsighted_grid)==2){
   nearsighted_grid  <- as.data.frame(nearsighted_grid[1])
 }
 
-#3. Function to determine downstream summations for nearsighted grid AKA Demands and Capacties 
+#Function to determine downstream summations for nearsighted grid AKA Demands and Capacties 
 nearsighted_grid_cumulatives <- downstream.sum.calculator(nearsighted_grid)
 
-#4. Far Sighted function to improve near-sighted greedy grid
-#**********Broken!*************
+#Far Sighted function to improve near-sighted greedy grid
+#***********************
 farsighted_grid <- far_sighted_rollout(nearsighted_grid_cumulatives)
 #*****
 
 #output csv for analysess 
-write.csv(farsighted_grid, "metrics-local-grid-only-rollout_sequence.csv", row.names=F)
+#write.csv(farsighted_grid, "testing-metrics-local-grid-only-rollout_sequence.csv", row.names=F)
 
-#PAss through ALL metric local values
+#merge metrics local with ranked grid nodes
 shared_column_names <- colnames(local)[which(colnames(local) %in% colnames(farsighted_grid))]
-farsighted_grid_full <- merge(local, farsighted_grid, by = shared_column_names, all.x=T, all.y=T)
-#Output it all with useful phases
-write.csv(farsighted_grid_full, "metrics-local-all-nodes-rollout_sequence.csv", row.names=F)
+all_nodes_ranked <- merge(local, farsighted_grid, by = shared_column_names, all=T)
+write.csv(all_nodes_ranked, "TEST-metrics-local-all-nodes-rollout_sequence.csv", row.names=F)
 
 # #Output a new shapefile with all the attirbute data of interest
 #remove multiple nodes on line segments
 metrics_local_with_sequence <- (farsighted_grid[which(!(duplicated(farsighted_grid$id))),])
 proposed_with_rollout <- merge(proposed, metrics_local_with_sequence, by.x = "FID", by.y = "id")
-writeLinesShape(proposed_with_rollout, "networks-proposed-with-rollout.shp")
+writeLinesShape(proposed_with_rollout, "TEST-networks-proposed-with-rollout.shp")
